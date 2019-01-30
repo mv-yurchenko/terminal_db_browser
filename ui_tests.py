ee61c67cf21b500ffb4165bdfeb1d34bdd0c9ba4
Maxim Yurchenko <mv.urchenko.dev@gmail.com>
2019-01-31 00:46:59 +0300

    Added tests

--- ui_tests.py
+++ ui_tests.py
@@ -7,16 +7,21 @@ class TestUI(unittest.TestCase):
     path = "sqlite_example.db"
     db = SQLite(path)
     ui = UI(db)
-
+    sql_commad_example_1 = "SELECT * FROM test_table"
+    sql_commad_example_2  = "SELECT test_text_field, test_int_field FROM test_table"
+    sql_commad_example_3  = "SELECT test_int_field FROM test_table"
     def test_get_command_type_and_command_text(self):
         ui_command_example = "/UI_COMMAND_EXAMPLE"
-        sql_commad_example = "SELECT * FROM TABLE"
 
-        self.assertEqual(("SQL_COMMAND", sql_commad_example), self.ui.get_command_type_and_command_text(sql_commad_example))
+
+        self.assertEqual(("SQL_COMMAND", self.sql_commad_example_1), self.ui.get_command_type_and_command_text(self.sql_commad_example_1))
         self.assertEqual(("UI_COMMAND", "UI_COMMAND_EXAMPLE"), self.ui.get_command_type_and_command_text(ui_command_example))
         self.assertEqual(ValueError, self.ui.get_command_type_and_command_text(""))
     
     def test_process_sql_command(self): 
-        pass
+        self.assertEqual([ "test_int_field", "test_text_field"], self.ui.process_sql_command_and_get_columns_names(self.sql_commad_example_1))
+        self.assertEqual([ "test_text_field" ,"test_int_field" ], self.ui.process_sql_command_and_get_columns_names(self.sql_commad_example_2))
+        self.assertEqual([ "test_int_field"], self.ui.process_sql_command_and_get_columns_names(self.sql_commad_example_3))
+
 if __name__ == "__main__":
     unittest.main()
